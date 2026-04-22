4d6020ec076585f46946a0fb83d25d8adb231bb1
chandan <153637267+chandan91077@users.noreply.github.com>
2026-04-22 14:52:58 +0530

    Time: 8 ms (53.03%), Space: 44.2 MB (41.65%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int[]a=new int[26];
+        int []b=new int[26];
+        for(int i=0;i<s1.length();i++){
+            a[s1.charAt(i)-'a']++;
+            b[s2.charAt(i)-'a']++;
+        }
+        if(count(a,b)){
+            return true;
+        }
+        for(int i=s1.length();i<s2.length();i++){
+            b[s2.charAt(i)-'a']++;
+            b[s2.charAt(i-s1.length())-'a']--;
+            if(count(a,b)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean count(int[]a,int []b){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+           
+        }
+         return true;
+    }
+}
