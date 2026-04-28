bad30d33d07e963a2a93496f26c097445f3f6f2b
chandan <153637267+chandan91077@users.noreply.github.com>
2026-04-29 01:41:43 +0530

    Time: 0 ms (100%), Space: 44.6 MB (68.7%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -7,21 +7,22 @@ class Solution {
         }
         if(i>=0){
             int j=n-1;
-            while(nums[j]<=nums[i]){
+            while(nums[i]>=nums[j]){
                 j--;
             }
             swap(nums,i,j);
         }
         reverse(nums,i+1,n-1);
     }
-    private static void swap(int[] nums,int i,int j){
+    private static void swap(int nums[],int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    private static void reverse(int[]nums,int i,int j){
+    private static void reverse(int nums[],int i,int j){
         while(i<j){
             swap(nums,i++,j--);
+
         }
     }
 }
