dce65abf3fa7b50038501c380f672755b443a393
chandan <153637267+chandan91077@users.noreply.github.com>
2026-05-23 19:52:35 +0530

    Time: 2 ms (76.02%), Space: 43.3 MB (81.21%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder str=new StringBuilder(s);
+        int i=0;
+        while(i<str.length()-1){
+            int diff=Math.abs(str.charAt(i)-str.charAt(i+1));
+            if(diff==32){
+                str.delete(i,i+2);
+                if(i>0)i--;
+            }
+            else i++;
+
+        }
+        return str.toString();
+    }
+}
